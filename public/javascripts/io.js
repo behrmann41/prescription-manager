ca65f836b5454648a095b7d3657ed6d428e3143a
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-20 12:55:53 -0700

    styled chat window that scrolls down as chat is added

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -1,11 +1,14 @@
 var socket = io()
 
 $('#chatform').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
 });
 
 socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-  });
+  var textdiv = $("#messagewindow");
+  var chatdiv = $("#chatwindow");
+  $('#messages').append($('<li>').text(msg));
+  textdiv.scrollTop(textdiv.prop('scrollHeight'));
+});
