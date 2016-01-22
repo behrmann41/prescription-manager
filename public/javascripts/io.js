31a5899ed2feb5d1643564b5e44a678560bbce7c
Ian Behrmann <ianjbehrmann@gmail.com>
2016-01-22 10:33:52 -0700

    chat now displays sender of message

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -1,14 +1,15 @@
 var socket = io()
 
 $('#chatform').submit(function(){
-  socket.emit('chat message', $('#m').val());
+  var name = $('.dropdown-menu li:first-child a').text();
+  socket.emit('chat message', $('#m').val(), name);
   $('#m').val('');
   return false;
 });
 
-socket.on('chat message', function(msg){
+socket.on('chat message', function(msg, personname){
   var textdiv = $("#messagewindow");
   var chatdiv = $("#chatwindow");
-  $('#messages').append($('<li>').text(msg));
+  $('#messages').append($('<li>').text(personname + " says " + msg));
   textdiv.scrollTop(textdiv.prop('scrollHeight'));
 });
